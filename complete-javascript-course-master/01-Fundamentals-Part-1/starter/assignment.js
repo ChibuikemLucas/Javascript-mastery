3dddbbfd359387dd97c1547faa3b162b8a379ec7
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-07-21 00:03:50 +0100

    feat: Do practice assignment on JS values and variables.
    Signed-off-by: ChibuikemLucas <chibuikemlucas@gmail.com>

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,11 @@
+// 1) Values and Variables﻿
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+//Log their values to the console.
+let country = "Nigeria";
+let continent = "Africa";
+let population = 223.8; // Population in millions
+
+console.log(country);
+console.log(continent);
+console.log(population);
