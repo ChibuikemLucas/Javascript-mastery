a80dfb0cb6cc63f2f1afd6fa064ebf5393eb5a7f
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-08-05 13:04:06 +0100

    topic: STRING AND TEMPLATE LITERALS
    Signed-off-by: ChibuikemLucas <chibuikemlucas@gmail.com>

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -6,3 +6,23 @@ console.log(23);
 
 let firstName = "Jonas";
 console.log(firstName);
+
+/* STRING AND TEMPLATE LITERALS
+const firstName2 = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2023;
+
+const jonas = "I'm " + firstName2 + ", a " + (year - birthYear) + " years old " + job + "!";
+const jonasNew = `I'm ${firstName2}, a ${year - birthYear} years old ${job}!`;
+
+//console.log(jonas);
+//console.log(jonasNew);
+
+// To create a multiline string
+//Without template literals
+console.log('String with \n\ multiple \n\ lines');
+//With template literals
+console.log(`String with 
+multiple
+lines`); */
